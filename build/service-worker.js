d4954bee3b9c0ec8e8e3e89bae5762a9f8a9fe17
samanthavmac <samanthavaleriemac@gmail.com>
2024-07-19 00:15:26 -0400

    test fix

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc9f50decd1349d25147b7c38737b6d2.js"
+  "/precache-manifest.0349b3218db22ca0e1fbaf47c301e7f0.js"
 );
 
 self.addEventListener('message', (event) => {
